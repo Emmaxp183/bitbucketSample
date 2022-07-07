55a630ae370b56c33222d575e160733be2f2231a
Emmanuel Mensah <41510090+Emmaxp183@users.noreply.github.com>
2022-07-08 01:27:57 +0200

    Initial Commit

---
+++ bitbucketSample/bitbucketSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitbucketSampleApp.swift
+//  bitbucketSample
+//
+//  Created by macbook on 08/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct bitbucketSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
